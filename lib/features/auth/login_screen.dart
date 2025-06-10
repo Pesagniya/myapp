59f963bc94c804b2553bca1d743942ceac90916a
Guilherme <guilhermeiwasaki@hotmail.com>
2025-06-10 03:55:28 -0300

    just comments

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -22,30 +22,3 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
-
-/*
-import 'package:flutter/material.dart';
-import 'package:flutter_login/flutter_login.dart';
-import 'package:myapp/features/rides/your_rides.dart';
-import 'package:myapp/features/auth/auth_service.dart';
-
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FlutterLogin(
-      logo: const AssetImage('assets/images/trademark.png'),
-      onLogin: AuthService.authUser,
-      onSignup: AuthService.signupUser,
-      onSubmitAnimationCompleted: () {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => const YourRidesScreen()),
-        );
-      },
-      onRecoverPassword: AuthService.recoverPassword,
-      theme: LoginTheme(logoWidth: 100, primaryColor: Colors.red),
-    );
-  }
-}
-*/
